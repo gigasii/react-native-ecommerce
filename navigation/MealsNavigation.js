0105f1f071ced0ff08fcb4b664ae946220155f20
Giggs <gigasii2856@gmail.com>
2021-06-29 15:45:50 +0800

    Added header navigation icons

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -1,9 +1,13 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 // Custom imports
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealScreen";
+import FavouritesScreen from "../screens/FavoritesScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 
 const MealsNavigator = createStackNavigator(
@@ -24,4 +28,30 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => (
+          <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+        ),
+      },
+    },
+    Favorites: {
+      screen: FavouritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => (
+          <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
